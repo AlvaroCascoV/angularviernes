ade26d337ad740ecaee8984a6926f8a2b01bf361
AlvaroCascoV <alvarocascovalero@gmail.com>
2025-11-07 14:15:29 +0100

    axios y fetch

--- src/app/components/personas-component/personas-component.ts
+++ src/app/components/personas-component/personas-component.ts
@@ -12,11 +12,23 @@ import { ServicePersonas } from '../../services/service.personas';
 })
 export class PersonasComponent {
   public personas!: Array<Persona>;
+  public personasAxios!: Array<Persona>;
+  public personasFetch!: Array<Persona>;
   constructor(private _service: ServicePersonas) {}
   ngOnInit(): void {
     this._service.getPersonas().then((response) => {
-      console.log('Leyendo');
+      console.log('Leyendo HTTP');
       this.personas = response;
+    });
+
+    this._service.getPersonasAxios().then((response) => {
+      console.log('Leyendo Axios');
+      this.personasAxios = response;
+    });
+
+    this._service.getPersonasFetch().then((response) => {
+      console.log('Leyendo Fetch');
+      this.personasFetch = response;
     });
   }
 }
--- src/app/services/service.personas.ts
+++ src/app/services/service.personas.ts
@@ -1,11 +1,34 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment.development';
+import axios from 'axios';
 
 @Injectable()
 export class ServicePersonas {
   constructor(private _http: HttpClient) {}
   getPersonas(): Promise<any> {
+    let urlApi = environment.urlApiPersonas;
+    let request = 'api/personas';
+    let promise = new Promise((resolve) => {
+      this._http.get(urlApi + request).subscribe((response) => {
+        resolve(response);
+      });
+    });
+    return promise;
+  }
+
+  getPersonasAxios(): Promise<any> {
+    let urlApi = environment.urlApiPersonas;
+    let request = 'api/personas';
+    let promise = new Promise((resolve) => {
+      axios.get(urlApi + request).then((response) => {
+        resolve(response.data);
+      });
+    });
+    return promise;
+  }
+
+  getPersonasFetch(): Promise<any> {
     let urlApi = environment.urlApiPersonas;
     let request = 'api/personas';
     let promise = new Promise((resolve) => {
